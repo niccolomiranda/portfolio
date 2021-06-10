d9d87bfc528edda12b03ef548af7da04e5f334ff
Niccolò Miranda <info@niccolomiranda.com>
2021-06-10 13:49:04 +0200

    Update paper-curtain.mjs

--- paper-curtain.mjs
+++ paper-curtain.mjs
@@ -5,9 +5,7 @@
 //not allowed to take the resource "as-is" and sell it,
 //redistribute, re-publish it, or sell "pluginized" versions of it.
 //Free plugins built using this resource should have a visible mention
-//and link to the original work. Always consider the licenses of all
-//included libraries, scripts and images used.
-
+//and link to the original work.
 
 
 import {
